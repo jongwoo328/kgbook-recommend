40b8465e3f418a369980bf55b35d2db62cedf7ad
Lee Jongwoo <jongwoo328@gmail.com>
2025-06-08 14:56:43 +0900

    스크립트용 db host 수정

--- aladin-mcp-server/scripts/migration.js
+++ aladin-mcp-server/scripts/migration.js
@@ -6,7 +6,7 @@ const sqlite = new Database('./db.sqlite');
 const pgclient = new pg.Client({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
+	host: 'localhost',
 	port: process.env.DB_PORT,
 	database: process.env.DB_NAME,
 });
