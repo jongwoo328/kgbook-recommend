5eddf98b521378c2bb7d62229f9c473ffde11e7a
Lee Jongwoo <jongwoo328@gmail.com>
2025-06-16 21:28:43 +0900

    primevue 추가

--- kgbook-recommend-web/nuxt.config.ts
+++ kgbook-recommend-web/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Nora from "@primeuix/themes/nora";
+import { definePreset } from "@primeuix/themes";
+
+const CustomPreset = definePreset(Nora, {
+  semantic: {
+    primary: {
+      50: "{teal.50}",
+      100: "{teal.100}",
+      200: "{teal.200}",
+      300: "{teal.300}",
+      400: "{teal.400}",
+      500: "{teal.500}",
+      600: "{teal.600}",
+      700: "{teal.700}",
+      800: "{teal.800}",
+      900: "{teal.900}",
+      950: "{teal.950}",
+    },
+  },
+});
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
@@ -7,8 +28,16 @@ export default defineNuxtConfig({
     "@nuxt/fonts",
     "@nuxt/eslint",
     "@nuxtjs/tailwindcss",
+    "@primevue/nuxt-module",
   ],
   devServer: {
     port: 8000,
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: CustomPreset,
+      },
+    },
   },
 });
