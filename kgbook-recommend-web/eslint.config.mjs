994933fa7c8c7c94a5b628ddcaf5f1b103d65117
Lee Jongwoo <jongwoo328@gmail.com>
2025-06-18 23:06:09 +0900

    nuxt eslint config 설정

--- kgbook-recommend-web/eslint.config.mjs
+++ kgbook-recommend-web/eslint.config.mjs
@@ -1,18 +1,17 @@
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
 import js from "@eslint/js";
 import tsEslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
-import nuxtEslintConfig from "@nuxt/eslint";
 import eslintPluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tsEslint.config(
-  {
-    extends: [
-      js.configs.recommended,
-      eslintPluginVue.configs["flat/recommended"],
-      eslintPluginVue.configs["flat/strongly-recommended"],
-      nuxtEslintConfig,
-    ],
-    files: ["**.*.{ts,vue}"],
-  },
+export default withNuxt(
+  js.configs.recommended,
+  tsEslint.configs.recommended,
+  eslintPluginVue.configs["flat/recommended"],
+  eslintPluginVue.configs["flat/strongly-recommended"],
   eslintConfigPrettier,
+  {
+    files: ["**/*.{ts,vue}"],
+  },
 );
