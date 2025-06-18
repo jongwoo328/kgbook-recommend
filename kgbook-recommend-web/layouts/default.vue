c0cfa78bcddf528e499527327b68c1eb865513da
Lee Jongwoo <jongwoo328@gmail.com>
2025-06-18 17:17:56 +0900

    nuxt 기본 설정 (#13)
    * tailwind 추가
    
    * primevue 추가
    
    * 폰트 설정
    
    * layout 추가
    
    * 브랜치 설명 추가
    
    * 최상위 readme 추가
    
    * 포인트색상 변수 추가
    
    * pr 템플릿 추가
    
    * docker 설정 추가
    
    * @nuxt/fonts 제거

---
+++ kgbook-recommend-web/layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="container mx-auto px-4">
+    <slot />
+  </main>
+</template>
+
+<style scoped></style>
--- kgbook-recommend-web/nuxt.config.ts
+++ kgbook-recommend-web/nuxt.config.ts
@@ -1,9 +1,57 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Nora from "@primeuix/themes/nora";
+import { definePreset } from "@primeuix/themes";
+
+const CustomPreset = definePreset(Nora, {
+  semantic: {
+    primary: {
+      50: "{teal.50}",
+      100: "{teal.100}",
+      200: "{teal.200}",
+      300: "{teal.300}",
+      400: "{teal.400}",
+      500: "{teal.500}",
+      600: "{teal.600}",
+      700: "{teal.700}",
+      800: "{teal.800}",
+      900: "{teal.900}",
+      950: "{teal.950}",
+    },
+  },
+});
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/icon", "@nuxt/fonts", "@nuxt/eslint"],
+  modules: [
+    "@nuxt/icon",
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@primevue/nuxt-module",
+  ],
   devServer: {
     port: 8000,
   },
+  primevue: {
+    options: {
+      theme: {
+        preset: CustomPreset,
+      },
+    },
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "ko",
+      },
+      link: [
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/reset.css", "~/assets/css/global.css"],
 });
