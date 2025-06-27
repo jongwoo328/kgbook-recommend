c4874a445a903ebfede1ed07830f5198b967d17b
Lee Jongwoo <jongwoo328@gmail.com>
2025-06-28 00:11:20 +0900

    임시

--- kgbook-recommend-web/server/api/chat.post.ts
+++ kgbook-recommend-web/server/api/chat.post.ts
@@ -1,1 +1,14 @@
-export default defineEventHandler(async (event) => {});
+import { agent } from "~/server/ai";
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody<ChatRequest>(event);
+
+  const r = await agent.invoke({
+    messages: body.messagesBefore.concat({
+      role: "user",
+      content: body.message,
+    }),
+  });
+  console.log(r);
+  return {};
+});
