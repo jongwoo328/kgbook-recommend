28fc060b73a6eb0253fab708414741a5fe110c42
Lee Jongwoo <jongwoo328@gmail.com>
2025-07-07 17:48:53 +0900

    로딩 사이즈 늘림

--- kgbook-recommend-web/components/FloatingAiChatAiLoading.vue
+++ kgbook-recommend-web/components/FloatingAiChatAiLoading.vue
@@ -5,7 +5,7 @@
     <div class="flex mt-2">
       <Icon class="text-2xl mr-2" name="hugeicons:ai-chat-02" />
       <div class="flex-1 flex items-center">
-        <Icon name="eos-icons:bubble-loading" />
+        <Icon class="text-3xl" name="eos-icons:bubble-loading" />
       </div>
     </div>
   </div>
