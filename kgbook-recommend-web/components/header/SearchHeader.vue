3c98fce6b238c75fe6c1d5399889695b4967a1f5
Lee Jongwoo <jongwoo328@gmail.com>
2025-07-20 20:48:40 +0900

    검색 ui 수정

--- kgbook-recommend-web/components/header/SearchHeader.vue
+++ kgbook-recommend-web/components/header/SearchHeader.vue
@@ -1,16 +1,35 @@
 <script lang="ts" setup>
-import IconField from "primevue/iconfield";
-
 import { ref } from "vue";
 
 const router = useRouter();
 const searchValue = ref<string>("");
+
+const categoryOptions = [
+  {
+    label: "제목+저자",
+    value: "Keyword",
+  },
+  {
+    label: "제목",
+    value: "Title",
+  },
+  {
+    label: "저자",
+    value: "Author",
+  },
+  {
+    label: "출판사",
+    value: "Publisher",
+  },
+];
+const selectedCategory = ref("Keyword");
 
 function searchBook() {
   router.push({
     path: "/search",
     query: {
       text: searchValue.value,
+      category: selectedCategory.value,
     },
   });
   console.log(`@@ searchBook search value=(${searchValue.value})`);
@@ -49,16 +68,27 @@ function searchBook() {
         주목할 만한 신간
       </NuxtLink>
     </div>
-    <IconField class="w-[70%] my-2">
-      <InputIcon>
-        <Icon class="text-xl" name="mdi-light:magnify" />
-      </InputIcon>
+    <div class="my-2 w-full flex justify-center items-center gap-2">
+      <Select
+        v-model="selectedCategory"
+        :options="categoryOptions"
+        class="border-2 rounded-3xl border-black dark:border-white flex items-center w-[10rem]"
+        option-label="label"
+        option-value="value"
+        size="large"
+      />
       <InputText
         v-model="searchValue"
-        class="w-full rounded-3xl px-8 py-4 border-2 border-black dark:border-white"
+        class="w-[30rem] rounded-3xl px-8 py-3 border-2 border-black dark:border-white"
         placeholder="검색하여 책을 찾아보세요."
         @keydown.enter="searchBook"
       />
-    </IconField>
+      <button
+        class="h-12 w-12 flex justify-center items-center rounded-3xl border-black dark:border-white"
+        @click="searchBook"
+      >
+        <Icon name="mdi-light:magnify" size="32" />
+      </button>
+    </div>
   </div>
 </template>
