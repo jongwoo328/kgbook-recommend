2e7dff834f7bff5454f90331be085437845a8836
김혜정(molly.ouo) <molly.ouo@kakaocorp.com>
2025-08-11 16:43:57 +0900

    로고 반영

--- kgbook-recommend-web/server/client/index.ts
+++ kgbook-recommend-web/server/client/index.ts
@@ -1,3 +1,5 @@
 import { Aladin } from "aladin-client";
 
-export const aladinClient = new Aladin({ ttbKey: process.env.TTB_KEY ?? "" });
+export const aladinClient = new Aladin({
+  ttbKey: process.env.TTB_KEY ?? "ttbkimhyejung121541001",
+});
